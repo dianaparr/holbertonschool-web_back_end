f58fd0cbef1443d0ff39cd6617829f6bfc85945e
dianaparr <dianaparra.9611@gmail.com>
2022-01-31 23:16:35 -0500

    Type checking and gitignore file

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Module name 102-type_checking """
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Use mypy to validate the following piece of code and apply any necessary changes """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array))
+
+zoom_3x = zoom_array(tuple(array), int(3.0))
