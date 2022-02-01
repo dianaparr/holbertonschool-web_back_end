18bf93404cd1190a2d9446e4d6be1caf0502792f
dianaparr <dianaparra.9611@gmail.com>
2022-01-31 21:54:06 -0500

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module name 4-define_variables
+
+    Define and annotate the following variables with the specified values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
