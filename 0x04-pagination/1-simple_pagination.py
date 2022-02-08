3f4a5a54be7b1c814f020fd025160f1fea549c0e
dianaparr <dianaparra.9611@gmail.com>
2022-02-07 22:09:37 -0500

    add task of simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" Module named 1-simple_pagination """
+
+
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ A method that get a page
+
+        Args:
+            page -> integer: 1
+            page_size -> integer: 10
+
+        Return: a list of lists
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+
+        pagination: List = self.dataset()
+        size_file = len(pagination)
+        range = index_range(page, page_size)
+        if (range[0] >= size_file):
+            return []
+
+        return (pagination[range[0]:range[1]])
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ The function should return a tuple of size two containing a
+        start index and an end index corresponding to the range of
+        indexes to return in a list for those particular pagination parameters.
+
+    Args:
+        page -> integer
+        page_size -> integer
+
+    Return: a tuple of size two containing a start index
+            and end index (int, int)
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
