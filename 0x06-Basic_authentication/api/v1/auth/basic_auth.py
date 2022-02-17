1271a67a8ccf2cbdc0a24d0153550de962fa63e2
dianaparr <dianaparra.9611@gmail.com>
2022-02-17 10:23:15 -0500

    Basic - User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,16 @@ class BasicAuth(Auth):
             return b64decode(base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ Method in the class BasicAuth that returns the user email and
+            password from the Base64 decoded value.
+        """
+        if (decoded_base64_authorization_header is None or
+                type(decoded_base64_authorization_header) is not str or
+                ':' not in decoded_base64_authorization_header):
+            return None, None
+        values_auth = decoded_base64_authorization_header.split(':', 1)
+        return values_auth[0], values_auth[1]
