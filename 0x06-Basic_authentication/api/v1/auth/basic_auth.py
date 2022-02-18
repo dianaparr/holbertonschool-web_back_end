c005eb6564757efb7e6f0c2caac08b1b57a4744b
dianaparr <dianaparra.9611@gmail.com>
2022-02-17 19:55:37 -0500

    Basic - User object

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,8 @@
 
 
 from api.v1.auth.auth import Auth
-from flask import request
-from typing import List, TypeVar
+from models.user import User
+from typing import TypeVar
 from base64 import b64decode
 
 
@@ -46,3 +46,23 @@ class BasicAuth(Auth):
             return None, None
         values_auth = decoded_base64_authorization_header.split(':', 1)
         return values_auth[0], values_auth[1]
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """ Method in the class BasicAuth that returns the User instance based
+            on his email and password.
+        """
+        if (user_email is None or type(user_email) is not str):
+            return None
+        if (user_pwd is None or type(user_pwd) is not str):
+            return None
+        try:
+            user_found = User.search({'email': user_email})
+        except Exception:
+            return None
+        # if user_pwd is not the password of the User instance found -> None
+        for user in user_found:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
