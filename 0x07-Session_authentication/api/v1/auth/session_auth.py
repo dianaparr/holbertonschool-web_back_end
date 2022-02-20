02a4205d844c4883bd7e97806f1039d52b1b95ec
dianaparr <dianaparra.9611@gmail.com>
2022-02-20 17:44:58 -0500

    create a new session id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,27 @@
 
 
 from api.v1.auth.auth import Auth
+from typing import Dict
+from uuid import UUID, uuid4
 
 
 class SessionAuth(Auth):
-    """ Class SessionAuth empty """
-    pass
+    """ Class SessionAuth for creating a new
+        authentication mechanism
+    """
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Instance method that creates a
+            session ID for a user_id.
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        new_session_id: str = str(uuid4())
+        self.user_id_by_session_id[new_session_id] = user_id
+
+        # the same user_id can have multiple Session ID - indeed,
+        # the user_id is the value in the dictionary
+        # user_id_by_session_id
+        return new_session_id
