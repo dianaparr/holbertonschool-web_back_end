8529d3511197438e63d60cc92be1843c2b5be32b
dianaparr <dianaparra.9611@gmail.com>
2022-02-20 17:32:29 -0500

    add empty session auth

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module named session_auth to the class Auth """
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class SessionAuth empty """
+    pass
