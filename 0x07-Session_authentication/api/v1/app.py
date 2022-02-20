f543ead2ceb6a96143f35ecd52d510a44eb67ffa
dianaparr <dianaparra.9611@gmail.com>
2022-02-20 18:25:03 -0500

    some changes

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -56,15 +56,14 @@ def before_request_method() -> None:
                      '/api/v1/forbidden/',
                      '/api/v1/auth_session/login/']
 
-    if auth.require_auth(path=request.path, excluded_paths=list_excluded):
-        if not auth.authorization_header(request)\
-                and not auth.session_cookie(request):
-            abort(401)
-        if (auth.authorization_header(request) is None):
-            abort(401)
-        if (auth.current_user(request) is None):
-            abort(403)
-        request.current_user = auth.current_user(request)
+    if not auth.require_auth(request.path, list_excluded):
+        return
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
