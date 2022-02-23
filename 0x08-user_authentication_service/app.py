f291aff41500d67d98f93a18fc6a873357655d24
dianaparr <dianaparra.9611@gmail.com>
2022-02-22 22:39:45 -0500

    log in

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ Module called app """
 
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,22 @@ def register_user():
         return jsonify({"email": f"{email}", "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login_sessions():
+    """ Implement a login function to respond to the POST /sessions route """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        sessionID = AUTH.create_session(email)
+        res = jsonify({
+            "email": email,
+            "message": "logged in"})
+        res.set_cookie('session_id', sessionID)
+        return res
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
