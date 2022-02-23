6a8de3ea7cf4fe9805b12d5cf3339a3b67573259
dianaparr <dianaparra.9611@gmail.com>
2022-02-22 21:13:42 -0500

    credentials validation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -30,3 +30,15 @@ class Auth:
         except NoResultFound:
             user = self._db.add_user(email, _hash_password(password))
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Try locating the user by email. If it exists, check the password
+            with bcrypt.checkpw. If it matches return True. In any other
+            case, return False.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(
+                    'utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
