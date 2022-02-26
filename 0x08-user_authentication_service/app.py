0d1227f63f1e76f41a540e1c374d83fb2ab0ba77
dianaparr <dianaparra.9611@gmail.com>
2022-02-25 21:20:57 -0500

    get reset password token

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -70,5 +70,18 @@ def get_user_profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """ Implement a get_reset_password_token function to respond
+        to the POST /reset_password route
+    """
+    try:
+        email = request.form.get('email')
+        token_ = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token_}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
