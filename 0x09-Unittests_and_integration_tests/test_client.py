d99ac6a577f06e4f8adb765fcc4ebe66ec1f7226
dianaparr <dianaparra.9611@gmail.com>
2022-03-02 22:25:53 -0500

    mocking a property

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
-from unittest.mock import patch, Mock
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -22,3 +22,12 @@ class TestGithubOrgClient(unittest.TestCase):
         get_org = GithubOrgClient(name_org)
         self.assertEqual(get_org.org, mock_url.return_value)
         mock_url.assert_called_once_with(endpoint)
+
+    def test_public_repos_url(self):
+        """ """
+        mock_url = "www.someurl.com"
+        the_payload = {"repos_url": mock_url}
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=the_payload)):
+            res = GithubOrgClient("somewhere")
+            self.assertEqual(res._public_repos_url, mock_url)
