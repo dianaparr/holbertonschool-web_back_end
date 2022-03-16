0d6aae03fb66bd22b5e3348d59efe65ed8b12698
dianaparr <dianaparra.9611@gmail.com>
2022-03-15 21:39:37 -0500

    1. Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,8 +2,10 @@
 """ Module to create Cache class """
 
 import redis
-from typing import Union
+from typing import Optional, Union, Callable
 import uuid
+import sys
+
 
 class Cache:
     """ Create a Cache class """
@@ -26,3 +28,28 @@ class Cache:
         # redis> SET mykey "Hello"
         self._redis.set(generate_key, data)
         return generate_key
+
+    def get(self, name_key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ A get method that take a key string argument and
+            an optional Callable argument named fn
+        """
+        value = self._redis.get(name_key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, name_key: str) -> str:
+        """ That will automatically parametrize Cache.get
+            with the correct conversion function.
+        """
+        value = self._redis.get(name_key)
+        return value.decode('utf-8')
+
+    def get_int(self, name_key: str) -> int:
+        """ That will automatically parametrize Cache.get
+            with the correct conversion function.
+        """
+        # To request the native byte order of the host system,
+        # use sys.byteorder as the byte order value
+        return int.from_bytes(name_key, sys.byteorder)
