99e61665994eb11a656370f20baa05aa9ec98697
dianaparr <dianaparra.9611@gmail.com>
2022-07-20 18:29:35 -0500

    add task 5

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+--Script that creates a trigger
+DELIMITER //
+CREATE TRIGGER `upd_email_check` BEFORE UPDATE ON `users`
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END; //
