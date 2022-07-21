dae0c5fa0141de5d705a3a4e275d0565824923ce
dianaparr <dianaparra.9611@gmail.com>
2022-07-21 18:54:53 -0500

    add task 0

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --Script that creates a table with some requirements
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`email` VARCHAR(255) NOT NULL UNIQUE,
+	`name` VARCHAR(255)
 );
