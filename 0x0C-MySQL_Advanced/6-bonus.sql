88dc3bd96396f17016b95b254f73c06c7214727e
dianaparr <dianaparra.9611@gmail.com>
2022-07-20 20:00:17 -0500

    add task 6

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,20 @@
+--Script that creates a stored procedure
+DELIMITER //
+CREATE PROCEDURE AddBonus(
+    IN `user_id` INT,
+    IN `project_name` VARCHAR(255),
+    IN `score` INT
+)
+BEGIN
+    INSERT INTO `projects`(`name`)
+    SELECT `project_name`
+    FROM DUAL
+    WHERE `project_name` NOT IN (
+        SELECT `name` FROM `projects`
+    );
+
+    INSERT INTO `corrections`(`user_id`, `project_id`, `score`)
+    VALUES (
+        `user_id`, (SELECT `id` FROM `projects` WHERE `name` = `project_name`), `score`
+    );
+END; //
