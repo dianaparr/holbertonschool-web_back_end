1f19140c0c5fe2929d074d4487304440dd6efe35
dianaparr <dianaparra.9611@gmail.com>
2022-07-21 20:03:40 -0500

    add new task 4

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,8 +1,10 @@
---Script that creates a trigger
+-- Script that creates a trigger
+-- Add task 4
+
 CREATE
 TRIGGER `triger_order`
 AFTER INSERT ON `orders`
 FOR EACH ROW
 UPDATE `items`
-SET quantity = quantity - NEW.number
+SET `quantity` = `quantity` - NEW.number
 WHERE `name` = NEW.item_name;
