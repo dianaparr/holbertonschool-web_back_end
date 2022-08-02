eb05647a8174929ad81176e96791f7cebaa6d4df
dianaparr <dianaparra.9611@gmail.com>
2022-08-01 21:59:08 -0500

    add task 1

---
+++ 0x0F-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+function getFullResponseFromAPI(success) {
+  const newPromise = new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return newPromise;
+}
+
+export default getFullResponseFromAPI;
