4b865be7e5083bdce63b1aeda382f75515f13eba
dianaparr <dianaparra.9611@gmail.com>
2022-08-02 22:12:27 -0500

    add task advanced one

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+}
+
+export default asyncUploadUser;
