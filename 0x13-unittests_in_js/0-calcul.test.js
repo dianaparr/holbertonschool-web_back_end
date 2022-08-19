80cc61a313a7cdd6c5a7d527c6a804feeac63b86
dianaparr <dianaparra.9611@gmail.com>
2022-08-18 22:37:28 -0500

    search bug task 0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,40 +1,22 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('sum of integers', () => {
-    assert.strictEqual(calculateNumber(14, 7), 21);
-    assert.strictEqual(calculateNumber(-5, -10), -15);
-    assert.strictEqual(calculateNumber(100, -36), 64);
+  it('checks the output', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-
-  it('with floats, you most round up', () => {
-    assert.strictEqual(calculateNumber(1.7, 3.7), 6);
-    assert.strictEqual(calculateNumber(2, 7.9), 10);
-    assert.strictEqual(calculateNumber(3.5, 3), 7);
-    assert.strictEqual(calculateNumber(2, 0.6), 3);
-    assert.strictEqual(calculateNumber(0.4, 3.5), 4);
-  });
-
-  it('must be a number, returns it rounded', () => {
-    assert.strictEqual(isNaN(calculateNumber(9.8)), true);
-    assert.strictEqual(isNaN(calculateNumber(7)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
-  });
-
   it('negative numbers', () => {
-    assert.strictEqual(calculateNumber(-1, 1), 0);
     assert.strictEqual(calculateNumber(-1, -1), -2);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(-1, 1), 0);
   });
-
-  it('should throw typeerror if either param cannot be coerced to a number', () => {
-    assert.throws(() => calculateNumber('betty'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers'
-    });
-    assert.throws(() => calculateNumber(2.4, 'cat'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers'
-    });
+  it('checks arguments', () => {
+    assert.strictEqual(isNaN(calculateNumber()), true);
+    assert.strictEqual(isNaN(calculateNumber(1)), true);
   });
 });
