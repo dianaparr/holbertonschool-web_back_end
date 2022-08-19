114786531295c7f5620979c838bedac779f4d574
dianaparr <dianaparra.9611@gmail.com>
2022-08-18 22:01:40 -0500

    add task 0

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('sum of integers', () => {
+    assert.strictEqual(calculateNumber(14, 7), 21);
+    assert.strictEqual(calculateNumber(-5, -10), -15);
+    assert.strictEqual(calculateNumber(100, -36), 64);
+  });
+
+  it('with floats, you most round up', () => {
+    assert.strictEqual(calculateNumber(1.7, 3.7), 6);
+    assert.strictEqual(calculateNumber(2, 7.9), 10);
+    assert.strictEqual(calculateNumber(3.5, 3), 7);
+    assert.strictEqual(calculateNumber(2, 0.6), 3);
+    assert.strictEqual(calculateNumber(0.4, -3.5), -3);
+  });
+
+  it('must be a number, returns it rounded', () => {
+    assert.strictEqual(isNaN(calculateNumber(9.8)), true);
+    assert.strictEqual(isNaN(calculateNumber(7)), true);
+  });
+});
