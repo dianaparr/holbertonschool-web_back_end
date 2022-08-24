a150b2d94c4a17dae7e731df2f8129e5a5f7843b
dianaparr <dianaparra.9611@gmail.com>
2022-08-23 21:19:47 -0500

    fix phone number

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -3,7 +3,7 @@ const kue = require('kue'),
 
 const jobCreator = queue
   .create('push_notification_code', {
-    phoneNumber: '33331568889',
+    phoneNumber: '4153518780',
     message: 'This is the code to verify your account',
   })
   .save((err) => {
